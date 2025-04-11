45db2d2ce9dae104a9f8876226557506ca041261
Pranav1823 <riyuk1823@gmail.com>
2025-04-11 22:54:21 +0530

    Add files via upload

---
+++ deepseek.py
@@ -0,0 +1,7 @@
+from transformers import pipeline
+
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+pipe = pipeline("text-generation", model="deepseek-ai/DeepSeek-V3-0324", trust_remote_code=True)
+pipe(messages)
